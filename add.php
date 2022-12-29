19b9abdf9521885eef8b18f06bda57f1733bf5e2
Rediet-Baynesagn <97876621+Rediet-Baynesagn@users.noreply.github.com>
2022-12-29 11:48:49 +0300

    Add files via upload

---
+++ add.php
@@ -0,0 +1,39 @@
+<?php include 'header.html'; ?>
+<div id="main-content">
+    <h2>Add New Item</h2>
+    <form class="post-form" action="savedata.php" method="post">
+        <div class="form-group">
+            <label>Product Name</label>
+            <input type="text" name="sname" required/>
+        </div>
+        <div class="form-group">
+            <label>Model</label>
+            <input type="text" name="saddress" required/>
+        </div>
+        <div class="form-group">
+            <label>Status</label>
+            <select name="class" required>
+                <option value="" selected disabled>Select Status</option>
+                <?php
+                include 'config.php';
+
+                $sql = "SELECT * FROM studentclass";
+                $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+                while($row = mysqli_fetch_assoc($result)){
+                ?>
+                <option value="<?php echo $row['cid']; ?>"><?php echo $row['cname']; ?> ></option>
+
+              <?php } ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label>Location/Owner</label>
+            <input type="text" name="sphone" required/>
+        </div>
+        <input class="submit" type="submit" value="Save"  />
+    </form>
+</div>
+</div>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$conn = mysqli_connect("localhost","root","","epa1","3306") or die("Connection Failed");
+
+?>
+
+
